787cef13caa82916728b16e7b3efcd729cd17614
rd <rushit.dawda@ambab.com>
2018-03-22 21:38:09 +0530

    header changes

---
+++ footer.php
@@ -0,0 +1,127 @@
+      <!-- footer start-->
+      <footer>
+         <div class="widget-area">
+            <div class="container">
+               <div class="row">
+                  <div class="col-md-4 col-sm-3">
+                     <div class="widget">
+                        <h3>About Us</h3>
+                         <a class="logo" href="index.html"><img src="img/logo.png" alt="Ad Center"></a>
+                        <p>
+                           Come aboard were expecting you. Love life's sweetest reward Let it flow it floats back to you. As long as we live its you and me baby. There ain't nothin' wrong with that.
+                        </p>
+                        <div class="social-icon">
+                           <ul class="list-inline">
+                              <li><a class="img-circle" href="#"><i class="fa fa-facebook"></i></a></li>
+                              <li><a class="img-circle active" href="#"><i class="fa fa-twitter"></i></a></li>
+                              <li><a class="img-circle" href="#"><i class="fa fa-google-plus"></i></a></li>
+                              <li><a class="img-circle" href="#"><i class="fa fa-linkedin"></i></a></li>
+                              <li><a class="img-circle" href="#"><i class="fa fa-behance"></i></a></li>
+                           </ul>
+                        </div>
+                     </div> <!-- .widget -->                       
+                  </div>
+                  <!-- col-md-3 -->
+                  <div class="col-md-2 col-sm-3">                        
+                     <div class="widget">
+                        <h3>Quick links</h3>
+                        <ul>
+                           <li><a href="#">Virtual Marketing</a></li>
+                           <li><a href="#">SEO Services</a></li>
+                           <li><a href="#">Web Analytics</a></li>
+                           <li><a href="#">Web Development</a></li>
+                           <li><a href="#">Pay-per-click</a></li>
+                           <li><a href="#">Blog Management</a></li>
+                           <li><a href="#">Content Management</a></li>
+                        </ul>
+                     </div> <!-- .widget -->                 
+                  </div>
+                  <!-- col-md-3 -->
+                  <div class="col-md-3 col-sm-3">
+                     <div class="widget">
+                        <h3>Latest News</h3>
+                        <ul class="recent-posts-list">
+                           <li>
+                              <img src="img/footer/recent1.png" alt="">
+                              <a href="#">Support systems and returns</a><span class="date primary-color">April 20, 2016</span>
+                           </li>
+                           <li>
+                              <img src="img/footer/recent2.png" alt="">
+                              <a href="#">Kinfolk said Jed move away</a><span class="date primary-color">April 20, 2016</span>
+                           </li>
+                           <li>
+                              <img src="img/footer/recent3.png" alt="">
+                              <a href="#">Some food and up through</a><span class="date primary-color">April 20, 2016</span>
+                           </li>
+                        </ul>
+                     </div> <!-- .widget -->                          
+                  </div>
+                  <!-- col-md-3 -->
+                  <div class="col-md-3 col-sm-3">
+                     <div class="widget">
+                        <h3>Contact Us</h3>
+                        <strong>Address</strong>
+                        <p>Adcenter, 06 Highley St,<br>Victoria, Australia.</p>
+                        <strong>Email Us</strong>
+                        <p>Info@Adcenter.Com<br>Support@Adcenter.Com</p>
+                        <strong>Call Us</strong>
+                        <p>+(10) 123 456 7896<br>+(10) 123 456 7896</p>
+                     </div> <!-- .widget -->                
+                  </div>
+                  <!-- col-md-3 -->
+               </div>
+               <!-- row -->
+            </div><!-- container -->
+         </div>
+         <div class="copyright-bar text-uppercase">
+            <div class="container">
+               <div class="row ">
+                  <div class="col-md-5 col-sm-5">
+                     <p>&copy; AdCenter Company 2016. All rights reserved</p>
+                  </div>
+                  <!-- col-md-4  -->
+                  <div class="col-md-2 col-sm-2 ">
+                     <div class="scroll-top ">
+                        <a href="#" class="back-to-top img-circle "><i class="fa fa-angle-up"></i></a>
+                     </div>
+                  </div>
+                  <!-- col-md-4 -->
+                  <div class="col-md-5 col-sm-5">
+                     <ul class="list-inline">
+                        <li><a href="index.html">Home</a></li>
+                        <li><a href="aboutus1.html">About</a></li>
+                        <li><a href="service1.html">Services</a></li>                        
+                        <li><a href="#">Terms & Conditions</a></li>
+                        <li><a href="#">Privacy policy</a></li>
+                     </ul>
+                  </div>
+                  <!-- col-md-4 -->
+               </div>
+               <!-- row -->
+            </div>
+            <!-- container -->
+         </div>
+         <!-- copyright-bar -->
+      </footer><!-- footer End-->
+      
+      <!-- Using Script -->
+      <script src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
+      <script src="js/jquery-1.12.0.min.js"></script>
+      <script src="js/plugins.js"></script>
+      <script src="js/bootstrap.min.js"></script>
+      <script src="js/owl.carousel.min.js"></script>
+      <script src="js/selectize.min.js"></script>
+      <script src="js/jquery.magnific-popup.min.js"></script>
+      <script src="js/waypoints.min.js"></script>
+      <script src="js/jquery.counterup.min.js"></script>
+      <script src="js/jquery.equalheights.min.js"></script>
+      <script src="js/imgLiquid-min.js"></script>
+      <script src="js/barGraph.js"></script>
+      <script src="js/isotope.pkgd.min.js"></script>
+      <script src="js/jquery.sliderPro.min.js"></script>
+      <script src="js/vegas.min.js"></script>
+      <script src="js/wow.min.js"></script>
+      <script src="js/menu.js"></script>
+      <script src="js/main.js"></script>
+   </body>
+</html>
---
+++ header.php
@@ -0,0 +1,175 @@
+<!doctype html>
+<html class="no-js" lang="">
+   <head>
+      <meta charset="utf-8">
+      <meta http-equiv="x-ua-compatible" content="ie=edge">
+      <title>AD Center</title>
+      <meta name="description" content="">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <!-- favicon -->
+      <link rel="shortcut icon" type="image/png" href="img/favicon.png">
+      <link href='https://fonts.googleapis.com/css?family=Poppins:400,600,700,500,300%7CSource+Sans+Pro%7CDosis:400,500,700%7CLato:400,600' rel='stylesheet'>
+      <link rel="stylesheet" href="css/bootstrap.min.css">
+      <link rel="stylesheet" href="css/font-awesome.css">
+      <link rel="stylesheet" href="css/animate.css">
+      <link rel="stylesheet" href="css/owl.carousel.css">
+      <link rel="stylesheet" href="css/owl.theme.default.min.css">
+      <link rel="stylesheet" href="css/selectize.bootstrap3.css">
+      <link rel="stylesheet" href="css/menu.css">
+      <link rel="stylesheet" href="css/pe-icon-7-stroke.css">
+      <link rel="stylesheet" href="css/magnific-popup.css">
+      <link rel="stylesheet" href="css/bar-graph.css">
+      <link rel="stylesheet" href="css/slider-pro.min.css">
+      <link rel="stylesheet" href="css/vegas.min.css">
+      <link rel="stylesheet" href="css/style.css">
+      <link rel="stylesheet" href="css/color.css">
+      <link rel="stylesheet" href="css/responsive.css">
+      <!-- modernizr -->
+      <script src="js/modernizr-2.8.3.min.js"></script>
+   </head>
+   <body>
+      <header class="header-style1">
+         <div class="topbar-info-area">
+           <div class="container">
+             <div class="row">
+               <div class="col-md-6 col-sm-6 col-xs-6">
+                 <div class="logo"><a href="#"><img src="img/logo.png" alt="Scissors"></a></div>
+               </div>
+               <div class="col-md-6 col-sm-6 col-xs-6 text-right">              
+                 <div class="topbar-info"><i class="fa fa-ellipsis-v"></i></div>
+               </div>
+             </div>
+           </div>
+         </div>  <!-- topbar-info-area -->
+       
+         <div class="logo-area">
+            <div class="container">
+               <div class="row">
+                  <div class="col-md-4">
+                     <a class="logo" href="index.php"><img src="img/logo.png" alt="Ad Center"></a>
+                  </div> 
+                  <div class="col-md-8">
+                     <div class="row site-contact-info">
+                        <div class="col-md-4 col-sm-4 col-xs-4">
+                           <div class="contact-info-inner">
+                              <div class="info-icon expanded"><i class="pe-7s-mail-open"></i></div>
+                              <p>Want An Quick Quote?</p>
+                              <P class="primary-color"><a href="#">INFO@ADCENTER.COM</a></P>
+                           </div>
+                           <!-- .site-contact-info -->
+                        </div>
+                        <div class="col-md-4 col-sm-4 col-xs-4">                           
+                           <div class="contact-info-inner">
+                              <div class="info-icon"><i class="pe-7s-phone"></i></div>                           
+                              <p>Call us now</p>
+                              <P class="primary-color"><a href="tel:1-8002345678">+(01) 123 456 7896</a></P>
+                           </div>
+                           <!-- .site-contact-info -->
+                        </div>
+                        <div class="col-md-4 col-sm-4 col-xs-4 text-right">
+                           <div class="contact-info-inner">
+                               <a href="contact.html" class="btn btn-primary">Get a Quote <i class="fa fa-long-arrow-right"></i></a>
+                           </div>
+                        </div>
+                     </div>
+                     <!-- .row -->
+                  </div>
+               </div>
+               <!-- row -->
+            </div>
+         </div><!-- logo-area -->
+
+
+         <div class="topbar-toggle">            
+            <div class="topbar-close">
+               <i class="pe-7s-close-circle"></i>
+            </div><!-- .topbar-close -->
+
+            <a class="logo" href="index.php"><img src="img/logo.png" alt="Ad Center"></a>
+
+            <div class="site-contact-info">
+               <div class="contact-info-inner">
+                  <div class="info-icon expanded"><i class="pe-7s-mail-open"></i></div>
+                  <p>Want An Quick Quote?</p>
+                  <P class="primary-color"><a href="#">INFO@ADCENTER.COM</a></P>
+               </div>  
+
+               <div class="contact-info-inner">
+                  <div class="info-icon"><i class="pe-7s-phone"></i></div>                           
+                  <p>Call us now</p>
+                  <P class="primary-color"><a href="tel:1-8002345678">+(01) 123 456 7896</a></P>
+               </div>
+
+               <div class="contact-info-inner">
+                   <a href="contact.html" class="btn btn-primary">Get a Quote <i class="fa fa-long-arrow-right"></i></a>
+               </div>
+            </div><!-- site-contact-info -->                 
+         </div>
+
+         <div class="nav-area sticky-header">
+            <div class="container">
+               <div class="header-nav">
+                  <div class="main-menu cssmenu">
+                     <ul class="menu list-inline">
+                        <li class="active">
+                           <a href="index.php">Home</a>
+                        </li>
+                        <li>
+                           <a href="aboutus2.php">About Us</a>
+                        </li>
+                        <li>
+                           <a href="service2.php">Our Services</a>
+                        </li>
+                        <li>
+                           <a href="casestudies-1.html">Projects</a>
+                           <ul>
+                              <li><a href="casestudies-2.html">Casestudies version 2</a></li>
+                              <li><a href="casestudies-3.html">Casestudies version 3</a></li>
+                              <li><a href="casestudies-single.html">Casestudies-Details</a></li>
+                           </ul>
+                        </li>
+                        <li>
+                           <a href="#">Pages</a>
+                           <ul>
+                              <li>
+                                 <a href="resources1.html">Resources</a>
+                                 <ul>
+                                    <li><a href="resources-single.html">Resources-Details</a></li>
+                                 </ul>
+                              </li>
+                              <li><a href="404.html">404</a></li>
+                           </ul>
+                        </li>                        
+                        <li><a href="pricing.html">Pricing</a></li>
+                        <li>
+                           <a href="blog1.html">Latest news</a>
+                           <ul>
+                              <li><a href="blog2.html">Blog version 2</a></li>
+                              <li><a href="blog-single.html">Blog-Single</a></li>                              
+                           </ul>
+                        </li>                        
+                        <li><a href="contact.html">Contact Us</a></li>                        
+                        <li class="search-box pull-right">
+                           <a href="#"><i class="fa fa-search"></i></a>
+                           <ul>
+                              <li>
+                                 <div class="search-form">
+                                    <div class="row">
+                                       <div class="col-md-8 col-sm-8 col-xs-8"><input type="text" placeholder="Search here..?" name="s" class="form-control"></div>
+                                       <div class="col-md-4 col-sm-4 col-xs-4"><input type="submit" class="btn btn-dark" value="Search" name="submit"></div>
+                                    </div>
+                                 </div>
+                              </li>
+                           </ul>
+                        </li>
+                        <!-- .search-box -->
+                     </ul>
+                     <!-- .menu -->
+                  </div>
+                  <!-- .main-menu -->
+               </div>
+               <!-- .header-nav -->
+            </div>
+            <!-- .container -->
+         </div>
+         <!-- .nav-area -->
